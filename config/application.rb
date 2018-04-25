25b056fb50ff41b3f8c670dbf76263791e85d671
Charlie Jonas <devel@charliejonas.co.uk>
2018-04-25 23:26:25 +0100

    Specify dedicated rails log file for consistency with Procodile

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,10 @@ module Roomballot
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
 
+    # Use a single named logfile for Rails
+    logfile = Rails.root.join('log', 'rails.log')
+    Rails.logger = Logger.new(logfile)
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
