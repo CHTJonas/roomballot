c50bbd7d954562d8064ee9749ad63687fc846b79
Charlie Jonas <charlie@charliejonas.co.uk>
2019-02-07 23:48:23 +0000

    Add a few database seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,1 +1,64 @@
+House.create(name: '1 Benson Street', size: 6, price: '£117-£137', information: %{5 standard rooms, 1 ensuite.
+Re-painted summer 2017.
+Living room area & dining area with tables and chairs.
+Back garden with bike racks - shared with 3 Benson St.
+1 Benson St is one of the closest houses to college, with great communal areas and some decent sized rooms.
+It also shares a garden and a wall with 3 Benson, so there's an opportunity to build an extended community.
+The kitchen is quite small, however, with only 2 fridges.})
+Image.create(url: 'https://media.fitzjcr.com/housing/architects-plans/1+3_benson_street_ground_floor.jpg', description: 'Ground floor plans for 1 + 3 Benson St', dwelling: House.last)
+Image.create(url: 'https://media.fitzjcr.com/housing/pictures/onebenson/1.jpg', description: 'Image 1', dwelling: House.last)
+Image.create(url: 'https://media.fitzjcr.com/housing/pictures/onebenson/2.jpg', description: 'Image 2', dwelling: House.last)
+Image.create(url: 'https://media.fitzjcr.com/housing/pictures/onebenson/3.jpg', description: 'Image 3', dwelling: House.last)
+Image.create(url: 'https://media.fitzjcr.com/housing/pictures/onebenson/4.jpg', description: 'Image 4', dwelling: House.last)
+Image.create(url: 'https://media.fitzjcr.com/housing/pictures/onebenson/5.jpg', description: 'Image 5', dwelling: House.last)
+Image.create(url: 'https://media.fitzjcr.com/housing/pictures/onebenson/6.jpg', description: 'Image 6', dwelling: House.last)
+Image.create(url: 'https://media.fitzjcr.com/housing/pictures/onebenson/7.jpg', description: 'Image 7', dwelling: House.last)
+Image.create(url: 'https://media.fitzjcr.com/housing/pictures/onebenson/8.jpg', description: 'Image 8', dwelling: House.last)
+Image.create(url: 'https://media.fitzjcr.com/housing/pictures/onebenson/9.jpg', description: 'Image 9', dwelling: House.last)
+Image.create(url: 'https://media.fitzjcr.com/housing/pictures/onebenson/10.jpg', description: 'Image 10', dwelling: House.last)
+
+House.create(name: '3 Benson Street', size: 6, price: '£117-£137', information: %{5 standard rooms, 1 ensuite.
+Re-painted summer 2017 with some new carpets
+Living room area.
+Seperate dining area with tables and chairs.
+Back garden with bike racks - shared with 1 Benson St.
+3 Benson St is one of the closest houses to college, with great communal areas and some decent sized rooms.
+It also shares a garden and a wall with 1 Benson, so there's an opportunity to build an extended community.
+The kitchen is quite small, however, with only 2 fridges.})
+Image.create(url: 'https://media.fitzjcr.com/housing/architects-plans/1+3_benson_street_ground_floor.jpg', description: 'Ground floor plans for 1 + 3 Benson St', dwelling: House.last)
+
+House.create(name: '8 Huntingdon Road', size: 9, price: '£117-137', information: %{9 standard rooms.
+Kitchen with table & chairs.
+Lockable covered bike store at front of house.
+One of the largest houses, and one of the closest to college.
+A large kitchen, and some big rooms.})
+Image.create(url: 'https://media.fitzjcr.com/housing/pictures/eighthuntingdon/1.jpg', description: 'Image 1', dwelling: House.last)
+Image.create(url: 'https://media.fitzjcr.com/housing/pictures/eighthuntingdon/2.jpg', description: 'Image 2', dwelling: House.last)
+Image.create(url: 'https://media.fitzjcr.com/housing/pictures/eighthuntingdon/3.jpg', description: 'Image 3', dwelling: House.last)
+Image.create(url: 'https://media.fitzjcr.com/housing/pictures/eighthuntingdon/4.jpg', description: 'Image 4', dwelling: House.last)
+Image.create(url: 'https://media.fitzjcr.com/housing/pictures/eighthuntingdon/5.jpg', description: 'Image 5', dwelling: House.last)
+Image.create(url: 'https://media.fitzjcr.com/housing/pictures/eighthuntingdon/6.jpg', description: 'Image 6', dwelling: House.last)
+Image.create(url: 'https://media.fitzjcr.com/housing/pictures/eighthuntingdon/7.jpg', description: 'Image 7', dwelling: House.last)
+Image.create(url: 'https://media.fitzjcr.com/housing/pictures/eighthuntingdon/8.jpg', description: 'Image 8', dwelling: House.last)
+Image.create(url: 'https://media.fitzjcr.com/housing/pictures/eighthuntingdon/9.jpg', description: 'Image 9', dwelling: House.last)
+Image.create(url: 'https://media.fitzjcr.com/housing/pictures/eighthuntingdon/10.jpg', description: 'Image 10', dwelling: House.last)
+Image.create(url: 'https://media.fitzjcr.com/housing/pictures/eighthuntingdon/11.jpg', description: 'Image 11', dwelling: House.last)
+
+House.create(name: '92 Huntingdon Road', size: 8, price: '£117-£137', information: %{7 standard rooms, 1 semi-ensuite.
+Kitchen with table & chairs.
+Covered bike store in back garden (shared with 94 Huntingdon Road)})
+Image.create(url: 'https://media.fitzjcr.com/housing/architects-plans/92+94_huntingdon_road_all_floors.jpg', description: 'Plans for 92 and 94 Huntingdon Road', dwelling: House.last)
+Image.create(url: 'https://media.fitzjcr.com/housing/pictures/ninetytwohuntingdon/kitchen.jpg', description: 'Kitchen of 92 Huntingdon Road', dwelling: House.last)
+Image.create(url: 'https://media.fitzjcr.com/housing/pictures/Room-1.jpg', description: 'Room 1', dwelling: House.last)
+Image.create(url: 'https://media.fitzjcr.com/housing/pictures/Room-21.jpg', description: 'Room 2 (angle 1)', dwelling: House.last)
+Image.create(url: 'https://media.fitzjcr.com/housing/pictures/Room-22.jpg', description: 'Room 2 (angle 2)', dwelling: House.last)
+Image.create(url: 'https://media.fitzjcr.com/housing/pictures/Room-4-1.jpg', description: 'Room 4', dwelling: House.last)
+Image.create(url: 'https://media.fitzjcr.com/housing/pictures/Room-4a.jpg', description: 'Room 4a', dwelling: House.last)
+Image.create(url: 'https://media.fitzjcr.com/housing/pictures/Room-51.jpg', description: 'Room 5', dwelling: House.last)
+Image.create(url: 'https://media.fitzjcr.com/housing/pictures/Room-61.jpg', description: 'Room 6 (angle 1)', dwelling: House.last)
+Image.create(url: 'https://media.fitzjcr.com/housing/pictures/Room-62.jpg', description: 'Room 6 (angle 2)', dwelling: House.last)
+
+
+
+
 User.create_with_group(name: 'Charlie Jonas', email: 'charlie@charliejonas.co.uk', crsid: 'chtj2', category: :first_year, admin: true)
