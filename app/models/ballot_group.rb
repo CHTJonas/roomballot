dea25620da808e7d94ef91f991d992d347592b23
Charlie Jonas <charlie@charliejonas.co.uk>
2019-02-14 21:45:26 +0000

    Add ballot group shuffle using Fisher–Yates

--- app/models/ballot_group.rb
+++ app/models/ballot_group.rb
@@ -15,4 +15,31 @@ class BallotGroup < ApplicationRecord
   belongs_to :owner, class_name: 'User', foreign_key: 'user_id'
   has_many :members, class_name: 'User'
   has_secure_token
+
+  validates :size, numericality: {
+    greater_than_or_equal_to: 1,
+    less_than_or_equal_to: 9
+  }
+
+  def size
+    self.members.count
+  end
+
+  # Shuffles all the groups in-place to make a randomly ordered ballot.
+  # See https://en.wikipedia.org/wiki/Fisher–Yates_shuffle
+  def self.shuffle(seed)
+    groups = BallotGroup.all.to_a
+    start_i = groups.length - 1
+    end_i = 1
+    prng = Random.new(seed)
+    (start_i..end_i).each do |i|
+      j = prng.rand(0..i)
+      groups[i], groups[j] = groups[j], groups[i]
+    end
+    ActiveRecord::Base.transaction do
+      groups.each_with_index do |group, index|
+        group.update(ballot_position: index + 1)
+      end
+    end
+  end
 end
